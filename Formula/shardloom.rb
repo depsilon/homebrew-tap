6f981df273770095bb2f7dccb3fd25c09a11f8b2
Dylan Heinrich <dylan@Dylans-Air.attlocal.net>
2026-06-16 19:08:47 -0500

    Update shardloom to v0.1.3

--- Formula/shardloom.rb
+++ Formula/shardloom.rb
@@ -1,8 +1,8 @@
 class Shardloom < Formula
   desc "Vortex-first no-fallback local compute engine technical preview"
   homepage "https://shardloom.io"
-  url "https://github.com/depsilon/shardloom/releases/download/v0.1.1/shardloom-99093904d923-source.tar.gz"
-  sha256 "b1bec2ea26d31e2fdcc1a638e9bb9a508a8037dbcea812165733732367765dba"
+  url "https://github.com/depsilon/shardloom/releases/download/v0.1.3/shardloom-c44a0a9da3f6-source.tar.gz"
+  sha256 "270c2a53e02a8bae039e9eab064d1cc560018cb2fdf8aaff05844898eb2e53f8"
   license "Apache-2.0"
   head "https://github.com/depsilon/shardloom.git", branch: "main"
 

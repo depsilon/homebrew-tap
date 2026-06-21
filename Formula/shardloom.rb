c5b1b40ea67f3caaf3b7b6ec46767be238d12a53
Dylan Heinrich <dylan@Dylans-Air.attlocal.net>
2026-06-21 06:12:23 -0500

    Update shardloom formula to v0.2.0

--- Formula/shardloom.rb
+++ Formula/shardloom.rb
@@ -1,16 +1,16 @@
 class Shardloom < Formula
   desc "Vortex-first no-fallback local compute engine technical preview"
   homepage "https://shardloom.io"
-  url "https://github.com/depsilon/shardloom/releases/download/v0.1.10/shardloom-7464854a0b40-source.tar.gz"
-  version "0.1.10"
-  sha256 "cb40dbb085f9e2e554df81c2d8a3fd8b4489990cd2dbf2df278b138797e540d2"
+  url "https://github.com/depsilon/shardloom/releases/download/v0.2.0/shardloom-010003bf96cc-source.tar.gz"
+  version "0.2.0"
+  sha256 "2e645b427216060b4ff41f18de0b2271f9b7a8d90109e847d6f47bf5c44e1fee"
   license "Apache-2.0"
   head "https://github.com/depsilon/shardloom.git", branch: "main"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args(path: "shardloom-cli")
+    system "cargo", "install", *std_cargo_args(path: "shardloom-cli"), "--features", "release-user-surfaces"
   end
 
   test do

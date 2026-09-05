6c0702aac33e307cf59bdebbc571910b50f65084
Dylan Heinrich <dylan@Dylans-Air.attlocal.net>
2026-09-05 12:28:59 -0500

    Use the release URL version under current Homebrew audit rules

--- Formula/shardloom.rb
+++ Formula/shardloom.rb
@@ -2,7 +2,6 @@ class Shardloom < Formula
   desc "Vortex-first no-fallback local compute engine technical preview"
   homepage "https://shardloom.io"
   url "https://github.com/depsilon/shardloom/releases/download/v0.2.3/shardloom-79f1ad6d9634-source.tar.gz"
-  version "0.2.3"
   sha256 "77979e2ef5cae19841706612eb0da71dd4274d17165ef7a74f0fe83bfd6c6b6d"
   license "Apache-2.0"
   head "https://github.com/depsilon/shardloom.git", branch: "main"
